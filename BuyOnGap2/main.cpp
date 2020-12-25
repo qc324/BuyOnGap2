5afbb9f7ef6f3e35af1d60b22b2c765a656507f6
Qiu Chen <qc324@nyu.edu>
2020-12-25 16:42:59 -0500

    Initial Commit

---
+++ BuyOnGap2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BuyOnGap2
+//
+//  Created by Qiu Chen on 12/25/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
